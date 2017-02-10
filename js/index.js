f9831a299806c6c81b3573cf11166154d62e85f1
Xuezheng Ma <xuezhma@gmail.com>
2017-02-10 13:24:19 -0800

    wait till loaded

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-$(".lang-py").removeClass('hidden_elem')
-$(".lang-cpp").addClass('hidden_elem')
-$("Ads API Python SDK").addClass('_42fs')
-const buttons = document.getElementsByTagName("button")
-const searchPythonText = "Ads API Python SDK"
-const searchPHPText = "Ads API PHP SDK"
-for (let i = 0; i < buttons.length; i++) {
-  const found = $(buttons[i])
-  found.removeClass("_42fs")
-  if (buttons[i].textContent === searchPythonText) {
+window.onload = () => {
+  $(".lang-py").removeClass('hidden_elem')
+  $(".lang-cpp").addClass('hidden_elem')
+  $("Ads API Python SDK").addClass('_42fs')
+  const buttons = document.getElementsByTagName("button")
+  const searchPythonText = "Ads API Python SDK"
+  const searchPHPText = "Ads API PHP SDK"
+  for (let i = 0; i < buttons.length; i++) {
     const found = $(buttons[i])
-    found.addClass("_42fs")
+    found.removeClass("_42fs")
+    if (buttons[i].textContent === searchPythonText) {
+      const found = $(buttons[i])
+      found.addClass("_42fs")
+    }
   }
 }
